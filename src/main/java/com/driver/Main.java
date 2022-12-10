4582d083483d5dff235a3e32d88488506198c658
v-teja <tejaviswa95@gmail.com>
2022-12-10 16:55:57 +0530

    getter setter added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly read = new RWOnly();
+       // read.name = "Viswa Teja";  //name has private access in com.driver.RWOnly
+        read.setName("Viswa Teja");
+        System.out.println(read.getName());
+    }
 }
